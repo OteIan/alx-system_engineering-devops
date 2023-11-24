6d4991caffb55b2937bbf516077f0147da1bee48
Ote Ian <oteian9@gmail.com>
2023-11-24 21:33:03 +0000

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Execute a command
-exec { 'Kill_process' :
-  command => 'usr/bin/pkill killmenow',
-  onlyif  => 'usr/bin/pgrep killmenow',
+exec { 'pkill killmenow' :
+  path    => '/bin/',
+  command => 'pkill killmenow',
   }
