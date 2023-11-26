03ee93b1b62a732246eba217ab0458aae9ef8929
Ote Ian <oteian9@gmail.com>
2023-11-26 17:21:27 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,18 @@
 #!/usr/bin/pup
 # Configure the SSH server to refuse authentication with a password
+
 include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure  => present,
   path    => '/etc/ssh/sshd_config',
-  line    => '	PasswordAuthentication no',
+  line    => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
-  path    => '/etc/ssh/sshd_config'
-  line    => '	IdentityFile ~/.ssh/school'
+  path    => '/etc/ssh/sshd_config',
+  line    => '	IdentityFile ~/.ssh/school',
   replace => true,
 }
