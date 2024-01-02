1fc27bdcfda53f97e5cb7ee0374f326e535e0587
Ote Ian <oteian9@gmail.com>
2024-01-02 17:20:22 +0300

    Export as CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Returns Todo list for a given employee id
+and axports data in the CSV format"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    id = argv[1]
+
+    user = requests.get(url + f"users/{id}").json()
+    todos = requests.get(url + f"todos", params={"userId": id}).json()
+
+    file_name = f"{id}.csv"
+    with open(file_name, "w") as f:
+        for i in todos:
+            f.write(f'"{id}", "{user["username"]}", "{i["completed"]}", "{i["title"]}"\n')
