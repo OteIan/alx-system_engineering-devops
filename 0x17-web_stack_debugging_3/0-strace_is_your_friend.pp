fef7346bd6105f48feb3fc2d8e9b88ebea4f4253
Ote Ian <oteian9@gmail.com>
2024-01-17 23:58:56 +0300

    Fix: Path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,7 +6,7 @@ file { '/var/www/html/wp-settings.php':
 exec { 'find-class':
     command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
     path    => '/bin:/usr/bin',
-    require => File['var/www/html/wp-settings.php'],
+    require => File['/var/www/html/wp-settings.php'],
 }
 
 service { 'apache2':
