05a4085f814c927dbdd2d65f4704554dc7dd8354
Ote Ian <oteian9@gmail.com>
2024-01-17 23:50:53 +0300

    Debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,15 @@
+file { '/var/www/html/wp-settings.php':
+    ensure => file,
+}
+
+exec { 'find-class':
+    command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+    path    => '/bin:/usr/bin',
+    require => File['var/www/html/wp-settings.php'],
+}
+
+service { 'apache2':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/var/www/html/wp-settings.php'],
+}
